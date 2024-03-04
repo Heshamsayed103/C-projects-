6b9238b12314d5097ffb7f5f15ec79833b237453
Hesham Sayed Abdul Rauof <153128708+Heshamsayed103@users.noreply.github.com>
2024-03-04 17:18:23 +0200

    Add files via upload

---
+++ Project 16/Project 16/Project 16.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int num1, num2,num3,num4;
+	cout << "Please Enter the numbers Here\n";
+	cin >> num1 >> num2;
+	
+	cout << "The Sum of this numbers is =" << num1 * num2<<num3*num4;
+	cin>> num3, num4;
+	return 0;
+}
+	
---
+++ Project 17/Project 17/Project 17.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "Bello Hello Elzeroo ";
+	cout << " Web Schoolll";
+	cout << " I Love Programming";
+	cout << " Too Much";
+	cout << " Specially C++ ";
+	return 0;
+}
