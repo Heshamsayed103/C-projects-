2764401f8a7c7740af6046d8defa9eafd07d766e
Hesham Sayed Abdul Rauof <153128708+Heshamsayed103@users.noreply.github.com>
2024-03-05 22:36:56 +0200

    Add files via upload

---
+++ project 2/Project2/Source.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main() {
+	double selious;
+	cout << "Please Enter the number : ";
+	cin >> selious;
+	cout << "The degree by fernhut = " << selious * 33.8;
+	return 0;
+}
