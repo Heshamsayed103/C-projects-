f5f2071c77fd94109551136cf280536eb26e2a13
Hesham Sayed Abdul Rauof <153128708+Heshamsayed103@users.noreply.github.com>
2024-03-13 07:02:46 +0200

    Add files via upload

---
+++ Project 3/Project 3/Project 3.cpp
@@ -0,0 +1,18 @@
+//project to convert ASCII to characters and characters to ASCII
+#include <iostream>
+using namespace std;
+int main()
+{
+	int H = 72;
+	char b = 'G';
+
+	cout << "Please inter the number or char " << endl;
+	cin >> H;
+	cout << "The character  by ASCII is " << char(H);
+	cin >> b;
+	cout << "The number by ASCII = " << int(71);
+
+	return 0;
+
+
+}
