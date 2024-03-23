6110b7e0ffdf8de032513791e7d2f31185a59570
Hesham Sayed Abdul Rauof <153128708+Heshamsayed103@users.noreply.github.com>
2024-03-23 13:36:55 +0200

    Add files via upload

---
+++ Project 5/Project 5.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <limits.h>
+#include <climits>
+using namespace std;
+int main()
+{
+    cout<<"The Maximum Number is =>"<<INT_MAX<<endl;
+    cout<<"The Minimum Number is =>"<<INT_MIN<<endl;
+    cout<<"The Maximum short integer Number is =>"<<SHRT_MAX<<endl;
+    cout<<"THe Minimum Short integer Number is =>"<<SHRT_MIN<<endl;
+
+    return 0;
+}
