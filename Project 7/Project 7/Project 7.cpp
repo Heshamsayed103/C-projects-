23bc551711cb0d43edf5ff140c6a605834d19650
Hesham Sayed Abdul Rauof <153128708+Heshamsayed103@users.noreply.github.com>
2024-04-04 15:34:46 +0200

    Add files via upload

---
+++ Project 7/Project 7/Project 7.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    //Print this Statements under each other 
+        cout << "LINE ONE" <<endl;
+        cout << "Line Two" <<endl;
+        cout << "Line Three" <<endl;
+        //Print this Statements under each other 
+        cout << "I'm learning programming" << endl << "With" << endl << "Elzero" << endl << "Web" << endl << "School" << endl;
+        cout << "|=============================|" << endl << "|=== Elzero Web School ===|" << endl << "|=== C++ ===|" << endl << "|=====================================|";
+        return 0;
+
+}
