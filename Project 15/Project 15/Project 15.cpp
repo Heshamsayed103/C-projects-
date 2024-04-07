effc16f00c0cd621b864c44532fa9e4e62af3f8f
Hesham Sayed Abdul Rauof <153128708+Heshamsayed103@users.noreply.github.com>
2024-04-07 19:33:06 +0200

    Add files via upload

---
+++ Project 15/Project 15/Project 15.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int num, sum;
+	cout << "Please Enter The Number here ";
+	cin>>num;
+	cout << "The Number Square is =" << num * num;
+
+	return 0;
+}
